d4dac393d0488bc700b75dfc76ba68d9456ebe1f
zhanghuaiyu <991446772@qq.com>
2017-07-26 15:52:19 +0800

    添加后台管理

---
+++ KWGene/Areas/AdminArea/Controllers/DefaultController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KWGene.Areas.AdminArea.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: AdminArea/Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ KWGene/Filter/RootFilter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KWGene.Filter
+{
+    public class RootFilter : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            if (filterContext.HttpContext.Session["user"] != null)
+            {
+               //string userID = filterContext.HttpContext.Session["UserID"].ToString();
+                int root = 1;
+                
+            }
+            else
+            {
+                filterContext.HttpContext.Response.Redirect("/Login/Login");
+                return;
+                //用户未登录
+            }
+        }
+    }
+}
