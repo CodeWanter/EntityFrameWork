a1f6f9dedde36eb3bf04c0f69bf1f512a5742e41
zhanghuaiyu <991446772@qq.com>
2017-07-31 09:26:17 +0800

    12

--- GeneModel/UserInfo.cs
+++ GeneModel/UserInfo.cs
@@ -12,15 +12,22 @@ namespace GeneModel
     using System;
     using System.Collections.Generic;
     
-    public partial class Phone
+    public partial class UserInfo
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
-        public WF_Role()
+        public UserInfo()
         {
-            this.UserList = new HashSet<WF_User>();
+            this.MenuInfo = new HashSet<MenuInfo>();
         }
     
         public int Id { get; set; }
-        public string Num { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+        public string Root { get; set; }
+        public System.DateTime Createtime { get; set; }
+    
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<MenuInfo> MenuInfo { get; set; }
     }
 }
